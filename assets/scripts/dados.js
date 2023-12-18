c0b4e01578c927ab3939f7c19c6957528b1817c2
Andressa Lima <gregorio.andressa.lima@gmail.com>
2023-12-18 11:54:10 -0300

    commit adicionando rn

--- assets/scripts/dados.js
+++ assets/scripts/dados.js
@@ -1,47 +1,91 @@
 const categorias = [
     {
-        "idCategoria": 1,
-        "Descricao": "Gestão",
+        idCategoria: 1,
+        Descricao: "Gestão",
     },
     {
-        "idCategoria": 2,
-        "Descricao": "Cliente",
+        idCategoria: 2,
+        Descricao: "Cliente",
     },
     {
-        "idCategoria": 3,
-        "Descricao": "RP",
+        idCategoria: 3,
+        Descricao: "RP",
     },
 ]
-const motivos=[
+
+const motivos = [
     {
-        "idMotivo": 1,
-        "Descricao": "Planejamento",
-        "idCategoria": 1
+        idMotivo: 1,
+        Descricao: "Planejamento",
+        idCategoria: 1
     },
     {
-        "idMotivo": 2,
-        "Descricao": "Financeiro",
-        "idCategoria": 1
+        idMotivo: 2,
+        Descricao: "Financeiro",
+        idCategoria: 1
     },
     {
-        "idMotivo": 3,
-        "Descricao": "Quebra de Máquina",
-        "idCategoria": 2
-    }
+        idMotivo: 3,
+        Descricao: "Quebra de Máquina",
+        idCategoria: 2
+    },
+    {
+        idMotivo: 4,
+        Descricao: "Funções",
+        idCategoria: 3
+    },
 ]
 
+const produtos = [
+    {
+        idProduto: 1,
+        Descricao: "Vassoura",
+        Estoque: 20,
+        EstoqueMinimo: 5,
+        Unidade: "Un",
+        Preco: 10.00,
 
-const produtos=[
-    {
-        "idProduto": 10,
-        "Descricao": "Papel A4",
-        "Estoque": 10,
-        "EstoqueMinimo": 5,
     },
     {
-        "idProduto": 20,
-        "Descricao": "Mel doce",
-        "Estoque": 5,
-        "EstoqueMinimo": 5,
+        idProduto: 2,
+        Descricao: "Fiação",
+        Estoque: 15,
+        EstoqueMinimo: 5,
+        Unidade: "Un",
+        Preco: 16,
     },
+    {
+        idProduto: 3,
+        Descricao: "Ventilador",
+        Estoque: 10,
+        EstoqueMinimo: 5,
+        Unidade: "Un",
+        Preco: 10,
+      },
+    
+]
+
+const departamentos = [
+    {
+        idDep: 10,
+        Descricao: "Sec. Educacao",
+        Responsavel: "José",
+        idFunc: 1,
+        idCargo: "Comissionado"
+    },
+    {
+        idDep: 30,
+        Descricao: "Sec. Trabalho",
+        Responsavel: "Luiz",
+        idFunc: 2,
+        idCargo: "Gestor"
+    },
+    {
+        idDep: 40,
+        Descricao: "NAT",
+        Responsavel: "Maria",
+        idFunc: 3,
+        idCargo: "Gerente"
+    },
+    
 ]
